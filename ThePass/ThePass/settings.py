e3ac39b5eaf037a44542260ce453e8353a8b2be0
miavien <ki.llisaan@gmail.com>
2024-05-15 13:50:53 +0700

    submitData готов

--- ThePass/ThePass/settings.py
+++ ThePass/ThePass/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ThePass',
+    'pereval',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- ThePass/pereval/views.py
+++ ThePass/pereval/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import *
 
 # Create your views here.
+@api_view(['POST'])
+def submitData(request):
+    if request.method == 'POST':
+        try:
+            serializer = PerevalSerializer(data=request.data)
+            if serializer.is_valid():
+                instance = serializer.save()
+                return Response({"status": 200, "message": "Отправлено успешно", "id": instance.id})
+            else:
+                return Response({"status": 400, "message": "Bad Request", "id": None})
+        except Exception as e:
+            return Response({"status": 500, "message": "Ошибка подключения к базе данных", "id": None})
